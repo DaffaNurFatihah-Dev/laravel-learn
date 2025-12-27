47f3885a24674d65751e95aa510e6c92b8cb03ed
DaffaNurFatihah-Dev <daffanurfatihahh@gmail.com>
2025-12-27 15:53:19 +0700

    buat layouts blade

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,16 @@
+<style>
+    .footer {
+        position: absolute;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        height: 40px;
+        color: white;
+        text-align: center;
+        font-weight: 500;
+        line-height: 40px
+    }
+</style>
+<div class="footer bg-primary">
+    <p>Footer by Daffa Nur Fatihah</p>
+</div>
